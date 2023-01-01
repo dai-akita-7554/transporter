ac13aecc504b4067d39abcbd352f5baa1b74ca80
dai-akita-7554 <116083539+dai-akita-7554@users.noreply.github.com>
2023-01-02 01:25:34 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,78 @@
+import time
+from pathlib import Path
+import shutil
+from transporter import Transporter, UserCancelException
+
+if Path('same_dir').exists():
+    shutil.rmtree('same_dir')
+try:
+    Transporter('same_dir', 'same_dir', threading=False)
+except UserCancelException as e:
+    print(e)
+
+try:
+    Transporter('from', 'test.py', threading=False)
+except NotADirectoryError as e:
+    print(e)
+
+if Path('noexist').exists():
+    shutil.rmtree('noexist')
+try:
+    Transporter('noexist', '', threading=False)
+except UserCancelException as e:
+    print(e)
+
+path_from = Path('from')
+path_to = Path('to')
+
+for i_dir in [path_from, path_to]:
+    for element in i_dir.iterdir():
+        if element.is_file():
+            element.unlink()
+        else:
+            shutil.rmtree(element)
+
+def make_file(file_name):
+    print(f'meke file: {file_name}')
+    with (path_from / file_name).open('w') as f:
+        for c in f'write: {file_name}':
+            f.write(c)
+            time.sleep(0.5)
+
+def charwise_copy(file_from, file_to):
+    with Path(file_from).open('r') as f:
+        data = f.read()
+
+    with Path(file_to).open('w') as f:
+        for c in data:
+            f.write(c)
+            time.sleep(1)
+
+file_names = [f'test{i}' for i in range(10)]
+
+try:
+    no_thread = Transporter(path_from, path_to, threading=False)
+    for file_name in file_names:
+        make_file(file_name)
+        no_thread.transport(file_name)
+    no_thread.shutdown()
+except UserCancelException as e:
+    print(e)
+
+path_from = Path('from')
+path_to = Path('to')
+
+for i_dir in [path_from, path_to]:
+    for element in i_dir.iterdir():
+        if element.is_file():
+            element.unlink()
+        else:
+            shutil.rmtree(element)
+
+try:
+    with Transporter(path_from, path_to, copy_method=charwise_copy) as transporter:
+        for file_name in file_names:
+            make_file(file_name)
+            transporter.transport(file_name)
+except UserCancelException as e:
+    print(e)
---
+++ transporter.py
@@ -0,0 +1,109 @@
+from pathlib import Path
+from shutil import copy2
+from concurrent.futures import ThreadPoolExecutor
+
+class UserCancelException(BaseException):
+    def __init__(self, msg='UserCancelException'):
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+    
+    def __repr__(self):
+        return self.msg
+
+class DummyExecutor:
+    def submit(self, fn, /, *args, **kwargs):
+        fn(*args, **kwargs)
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return False
+    
+    def shutdown(self, wait=True, *, cance_futures=False):
+        pass
+
+def fun_delete(file_name):
+    Path(file_name).unlink()
+
+class Transporter:
+    def __init__(self, path_from, path_to, confirm=True, 
+            copy_method=copy2, del_method=fun_delete, threading=True):
+
+        self.path_from = Path(path_from).resolve()
+        self.path_to = Path(path_to).resolve()
+
+        def decorate_copy(method):
+            def copy_method2(file_path_from, file_path_to):
+                print('copy file \n' \
+                    f'from {file_path_from} \n' \
+                    f'to {file_path_to}')
+                method(file_path_from, file_path_to)
+            return copy_method2
+        self.copy_method = decorate_copy(copy_method)
+
+        def decorate_del(method):
+            def del_method2(file_path_from):
+                print(f'delete file {file_path_from}')
+                method(file_path_from)
+            return del_method2
+        self.del_method = decorate_del(del_method)
+
+        if self.path_from == self.path_to:
+            print(f'from- and to-directory are the same: {self.path_from}. \n' \
+                'The transporter do nothing. Do you want to continue?')
+            if not self.respond_yes():
+                raise UserCancelException()
+
+        for path_use in [self.path_from, self.path_to]:
+            self.check_path(path_use)
+        
+        if confirm:
+            print('The file will be moved \n' \
+                f'from {self.path_from} \n' \
+                f'to {self.path_to} \n' \
+                'each time the method "transport" is called with a filename.')
+            print('Are you sure?')
+            if not self.respond_yes():
+                raise UserCancelException()
+        
+        if threading:
+            self.executor = ThreadPoolExecutor(max_workers=1)
+        else:
+            self.executor = DummyExecutor()
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.executor.__exit__(exc_type, exc_val, exc_tb)
+        return False
+    
+    def shutdown(self, *args, **kwargs):
+        self.executor.shutdown(*args, **kwargs)
+    
+    def respond_yes(self):
+        response = input('[y/n]:')
+        while response not in ['y', 'n']:
+            response = input('[y/n]:')
+        return response == 'y'
+    
+    def check_path(self, path_use):
+        if not path_use.exists():
+            print(f'The directory {path_use} does not exist. Do you want to make it?')
+            if self.respond_yes():
+                path_use.mkdir()
+            else:
+                raise UserCancelException()
+        elif path_use.is_file():
+            raise NotADirectoryError(path_use)
+
+    def transport(self, file_name):
+        file_path_from = self.path_from / file_name
+        file_path_to = self.path_to / file_name
+
+        self.executor.submit(self.copy_method, file_path_from, file_path_to)
+        self.executor.submit(self.del_method, file_path_from)
+
